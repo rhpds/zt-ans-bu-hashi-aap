8ae96f302787261e567d548828d9715c0e6ced7b
Nuno Martins <nmartins@redhat.com>
2025-11-25 19:34:18 +0200

    Update setup-terraform.sh

--- setup-automation/setup-terraform.sh
+++ setup-automation/setup-terraform.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "change me"
 
-HOSTNAME=$(hostname -f)
+HOSTNAME="tfe-https-${GUID}.${DOMAIN}"
 
 sed -i "/name: \"TFE_HOSTNAME\"/!b;n;s/value: \".*\"/value: \"$HOSTNAME\"/" /etc/containers/systemd/tfe.yaml
 
