1fd50f7c510468f92058ee81a29339b176dfc2f8
Wilson Harris <wharris@redhat.com>
2025-12-12 15:35:07 -0500

    Update setup-control, ifname to eth1, remove rhui repos

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -3,9 +3,13 @@
 systemctl stop systemd-tmpfiles-setup.service
 systemctl disable systemd-tmpfiles-setup.service
 
-nmcli connection add type ethernet con-name enp2s0 ifname enp2s0 ipv4.addresses 192.168.1.10/24 ipv4.method manual connection.autoconnect yes
-nmcli connection up enp2s0
+nmcli connection add type ethernet con-name eth1 ifname eth1 ipv4.addresses 192.168.1.10/24 ipv4.method manual connection.autoconnect yes
+nmcli connection up eth1
 echo "192.168.1.10 control.lab control" >> /etc/hosts
+
+rm -rf /etc/yum.repos.d/*
+yum clean all
+subcription-manager clean
 
 curl -k  -L https://${SATELLITE_URL}/pub/katello-server-ca.crt -o /etc/pki/ca-trust/source/anchors/${SATELLITE_URL}.ca.crt
 update-ca-trust
